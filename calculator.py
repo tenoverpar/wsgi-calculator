8ebe03b568acc6d0678ae8e943fed63569f7d660
Tracy A <tcpip201@gmail.com>
2018-07-24 18:10:05 -0700

    commit calculator and .gitignore file

--- calculator.py
+++ calculator.py
@@ -1,3 +1,12 @@
+#!/usr/bin/env python
+
+import traceback
+
+# Programming in python B Summer 2018
+# July 24, 2018
+# Mailroom Session 4 - wsgi-calculator
+# Tracy Allen - git repo https://github.com/tenoverpar/wsgi-calculator
+
 """
 For your homework this week, you'll be creating a wsgi application of
 your own.
@@ -23,7 +32,8 @@ Consider the following URL/Response body pairs as tests:
   http://localhost:8080/add/23/42      => 65
   http://localhost:8080/subtract/23/42 => -19
   http://localhost:8080/divide/22/11   => 2
-  http://localhost:8080/               => <html>Here's how to use this page...</html>
+  http://localhost:8080/               => <html>Here's how to use this
+                                                page...</html>
 ```
 
 To submit your homework:
@@ -42,16 +52,66 @@ To submit your homework:
 """
 
 
+def home():
+    """Page index with instructions."""
+    page = """
+    <h1>Here's how to use this calculator page...</h1>
+    <p>You perform basic math operations by visiting addresses like this:</p>
+      <p><a href="http://localhost:8080/multiply/3/5">http://localhost:8080/
+          multiply/3/5</a> => 15</p>
+      <p><a href="http://localhost:8080/add/23/42">http://localhost:8080/
+          add/23/42</a>  => 65</p>
+      <p><a href="http://localhost:8080/subtract/23/42">http://localhost:8080/
+          subtract/23/42</a>  => -19</p>
+      <p><a href="http://localhost:8080/divide/22/11">http://localhost:8080/
+          divide/22/11</a>  => 2</p>
+    """
+    return page
+
+
+def decorate_math(math_func):
+    """Provide decorating function for math."""
+    def return_func(*args):
+        """The math function output."""
+        template = """
+            <h1>{}</h1>
+            <p><br /></p>
+            <a href="/">Back to index</a>
+        """
+        try:
+            res = math_func(*args)
+        except ZeroDivisionError:
+            raise ZeroDivisionError
+        except ValueError:
+            raise ValueError
+        return template.format(res)
+
+    return return_func
+
+
+@decorate_math
 def add(*args):
-    """ Returns a STRING with the sum of the arguments """
+    """Return a STRING with the sum of arguments."""
+    return str(sum(map(float, args)))
 
-    # TODO: Fill sum with the correct value, based on the
-    # args provided.
-    sum = "0"
 
-    return sum
+@decorate_math
+def divide(*args):
+    """Return a STRING with the quotient of arguments."""
+    return str(float(args[0]) / float(args[1]))
 
-# TODO: Add functions for handling more arithmetic operations.
+
+@decorate_math
+def subtract(*args):
+    """Return a STRING with the subtract of arguments."""
+    return str(float(args[0]) - float(args[1]))
+
+
+@decorate_math
+def multiply(*args):
+    """Return a STRING with the product of arguments."""
+    return str(float(args[0]) * float(args[1]))
+
 
 def resolve_path(path):
     """
@@ -63,10 +123,25 @@ def resolve_path(path):
     # examples provide the correct *syntax*, but you should
     # determine the actual values of func and args using the
     # path.
-    func = add
-    args = ['25', '32']
+    funcs = {
+        '': home,
+        'add': add,
+        'divide': divide,
+        'subtract': subtract,
+        'multiply': multiply,
+        }
+
+    path = path.strip('/').split('/')
+    func_name = path[0]
+    args = path[1:]
+
+    try:
+        func = funcs[func_name]
+    except KeyError:
+        raise NameError
 
     return func, args
+
 
 def application(environ, start_response):
     # TODO: Your application code from the book database
@@ -76,9 +151,40 @@ def application(environ, start_response):
     #
     # TODO (bonus): Add error handling for a user attempting
     # to divide by zero.
-    pass
+    headers = [('Content-type', 'text/html')]
+    try:
+        path = environ.get('PATH_INFO', None)
+        if path is None:
+            raise NameError
+        func, args = resolve_path(path)
+        body = func(*args)
+        status = "200 OK"
+    except NameError:
+        status = "404 Not Found"
+        body = "<h1>Not Found</h1>"
+    except ZeroDivisionError:
+        status = "500 ZeroDivisionError"
+        body = "<h1>Can't divide by zero</h1>"
+        body += '<p><br /></p>'
+        body += '<a href="/">Back to index</a>'
+    except ValueError:
+        status = "500 ValueError"
+        body = "<h1>Try using numbers</h1>"
+        body += '<p><br /></p>'
+        body += '<a href="/">Back to index</a>'
+    except Exception:
+        status = "500 Internal Server Error"
+        body = "<h1>Internal Server Error</h1>"
+        print(traceback.format_exc())
+    finally:
+        headers.append(('Content-length', str(len(body))))
+        start_response(status, headers)
+        return [body.encode('utf8')]
+
 
 if __name__ == '__main__':
     # TODO: Insert the same boilerplate wsgiref simple
     # server creation that you used in the book database.
-    pass
+    from wsgiref.simple_server import make_server
+    srv = make_server('localhost', 8080, application)
+    srv.serve_forever()
